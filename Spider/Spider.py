1e6f19b39bc7dbb341a9b5bc5be1efe937d0e718
WangYunpeng <WangYunpeng@cag.com.hk>
2016-02-15 15:50:04 +0800

    获取指定URL内容并转码

---
+++ Spider/Spider.py
@@ -0,0 +1,14 @@
+﻿#coding=utf-8
+import urllib.request
+
+def getHtml(url):
+    page = urllib.request.urlopen(url)
+    html = page.read()
+    return html
+
+html = getHtml("http://www.sina.com.cn")
+
+print(html)
+print(type(html))
+html_utf8 = html.decode("UTF-8")
+print(html_utf8)
